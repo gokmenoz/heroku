1e826b21abf07cbe2f589fc841ec029ed3b7fa3a
gokmenoz <38671244+gokmenoz@users.noreply.github.com>
2018-05-11 11:19:36 -0400

    change

--- deploy.py
+++ deploy.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return app.send_static_file('index.html')
+    return app.send_static_file('plot1.html')
 
 if __name__ == '__main__':
     app.debug = False
